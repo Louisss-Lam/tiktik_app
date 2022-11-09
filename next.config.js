b919effcaeee981f53f479ac4b6c8282968739fa
Louisss-Lam <louislam617@gmail.com>
2022-11-09 17:12:44 +0000

    feat: videocard setup

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: [
+      "play-lh.googleusercontent.com",
+      'cdn.pixabay.com',
+      'p16-amd-va.tiktokcdn.com',
+      'image.shutterstock.com'
+    ],
+  }
 }
 
 module.exports = nextConfig
