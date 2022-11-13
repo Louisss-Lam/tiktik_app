5298d7431d9f27d87d19110349003b5b18887cea
Louisss-Lam <louislam617@gmail.com>
2022-11-13 17:33:22 +0000

    feat: create handle post function

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -13,6 +13,13 @@ const Upload = () => {
     const [isLoading, setIsLoading] = useState(false);
     const [videoAsset, setVideoAsset] = useState<SanityAssetDocument | undefined>();
     const [wrongFileType, setWrongFileType] = useState(false);
+    const [caption, setCaption] = useState('');
+    const [category, setCategory] = useState(topics[0].name);
+    const [savingPost, setSavingPost] = useState(false);
+
+    const { userProfile }: { userProfile: any } = useAuthStore();
+    const router = useRouter();
+
 
     const uploadVideo = async (e: any) => {
         const selectedFile = e.target.files[0];
@@ -33,9 +40,37 @@ const Upload = () => {
         }
     }
 
+    const handlePost = async () => {
+        if(caption && videoAsset?._id && category) {
+            setSavingPost(true);
+
+            const document = {
+                _type: 'post',
+                caption,
+                video: {
+                    _type: 'file',
+                    asset: {
+                        _type: 'reference',
+                        _ref: videoAsset?._id
+                    },
+                },
+                userId: userProfile?._id,
+                postedBy: {
+                    _type: 'postedBy',
+                    _ref: userProfile?._id
+                },
+                topic: category
+            };
+
+            await axios.post('http://localhost:3000/api/post', document);
+
+            router.push('/');
+        }
+    };
+
   return (
     <div className='flex w-full h-full absolute left-0 top-[60px] mb-10 pt-10 lg:pt-20 bg-[#F8F8F8] justify-center'>
-        <div className='bg-white rounded-lg xl:h-[80vh] flex gap-6 flex-wrap justify-center items-center p-14 pt-6'>
+        <div className='bg-white rounded-lg xl:h-[80vh] w-[60%] flex gap-6 flex-wrap justify-between items-center p-14 pt-6'>
             <div>
                 <div>
                     <p className='text-2xl font-bold'>Upload Video</p>
@@ -103,13 +138,13 @@ const Upload = () => {
                 </label>
                 <input
                     type='text'
-                    value=''
-                    onChange={() => {}}
+                    value={caption}
+                    onChange={(e) => setCaption(e.target.value)}
                     className='rounded outline-none text-md border-2 border-gray-200 p-2'
                 />
                 <label className='text-md font-medium'>Choose a Category</label>
                 <select
-                    onChange={() => {}}
+                    onChange={(e) => setCategory(e.target.value)}
                     className='outline-none border-2 border-gray-200 text-md capitalize lg:p-4 p-2 rounded cursor-pointer'
                 >
                     {topics.map((topic) => (
@@ -131,7 +166,7 @@ const Upload = () => {
                         Discard
                     </button>
                     <button
-                        onClick={() => {}}
+                        onClick={handlePost}
                         type="button"
                         className='bg-[#F51997] text-white text-md font-medium p-2 rounded w-28 lg:w-44 outline-none'
                     >
